a3fc658b55c442ca40fd80e546fa6f57d21beb72
dogmatic69 <dogmatic69@gmail.com>
2013-01-21 08:25:27 +0000

    doc blocks

--- Lib/DebugKitEvents.php
+++ Lib/DebugKitEvents.php
@@ -1,62 +1,94 @@
 <?php
-	/*
-	 * Short Description / title.
-	 *
-	 * Overview of what the file does. About a paragraph or two
-	 *
-	 * Copyright (c) 2010 Carl Sutton ( dogmatic69 )
-	 *
-	 * @filesource
-	 * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
-	 * @link http://www.infinitas-cms.org
-	 * @package {see_below}
-	 * @subpackage {see_below}
-	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
-	 * @since {check_current_milestone_in_lighthouse}
-	 *
-	 * @author {your_name}
-	 *
-	 * Licensed under The MIT License
-	 * Redistributions of files must retain the above copyright notice.
-	 */
+/*
+* Short Description / title.
+*
+* Overview of what the file does. About a paragraph or two
+*
+* Copyright (c) 2010 Carl Sutton ( dogmatic69 )
+*
+* @filesource
+* @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
+* @link http://www.infinitas-cms.org
+* @package {see_below}
+* @subpackage {see_below}
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @since {check_current_milestone_in_lighthouse}
+*
+* @author {your_name}
+*
+* Licensed under The MIT License
+* Redistributions of files must retain the above copyright notice.
+*/
 
-	final class DebugKitEvents extends AppEvents {
-		public function onAttachBehaviors(Event $Event) {
-			if(!Configure::read('debug')) {
-				return array();
-			}
+class DebugKitEvents extends AppEvents {
 
-			if(is_subclass_of($Event->Handler, 'Model')) {
-				$Event->Handler->Behaviors->attach('DebugKit.Timed');
-			}
+/**
+ * Attach behaviors
+ *
+ * @param Event $Event
+ *
+ * @return void
+ */
+	public function onAttachBehaviors(Event $Event) {
+		if(!Configure::read('debug')) {
+			return;
 		}
 
-		public function onRequireComponentsToLoad(Event $Event) {
-			if(!Configure::read('debug')) {
-				return array();
-			}
-
-			return 'DebugKit.Toolbar';
-		}
-
-		public function onRequireCssToLoad(Event $Event, $data = null) {
-			if(!Configure::read('debug')) {
-				return array();
-			}
-
-			return array(
-				'DebugKit.debug_toolbar'
-			);
-		}
-
-		public function onRequireJavascriptToLoad(Event $Event, $data = null) {
-			if(!Configure::read('debug')) {
-				return array();
-			}
-
-			return array(
-				'DebugKit.js_debug_toolbar',
-				'DebugKit.debug_kit'
-			);
+		if(is_subclass_of($Event->Handler, 'Model')) {
+			$Event->Handler->Behaviors->attach('DebugKit.Timed');
 		}
 	}
+
+/**
+ * Components to load
+ *
+ * @param Event $Event
+ *
+ * @return void|array
+ */
+	public function onRequireComponentsToLoad(Event $Event) {
+		if(!Configure::read('debug')) {
+			return;
+		}
+
+		return array(
+			'DebugKit.Toolbar'
+		);
+	}
+
+/**
+ * css to load
+ *
+ * @param Event $Event
+ *
+ * @return void|array
+ */
+	public function onRequireCssToLoad(Event $Event, $data = null) {
+		if(!Configure::read('debug')) {
+			return;
+		}
+
+		return array(
+			'DebugKit.debug_toolbar'
+		);
+	}
+
+/**
+ * js to load
+ *
+ * @param Event $Event
+ *
+ * @return void|array
+ */
+	public function onRequireJavascriptToLoad(Event $Event, $data = null) {
+		if(!Configure::read('debug')) {
+			return;
+		}
+
+		return array(
+			'DebugKit.jquery',
+			'DebugKit.js_debug_toolbar',
+			'DebugKit.debug_kit'
+		);
+	}
+}
